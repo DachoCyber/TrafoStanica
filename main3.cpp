070bb8500b70664fe00c4f2173321c1dd4f03703
DachoCyber <76927298+DachoCyber@users.noreply.github.com>
2025-02-14 19:39:10 +0100

    Add files via upload

--- main3.cpp
+++ main3.cpp
@@ -7,6 +7,10 @@
 #include <map>
 #include <queue>
 
+std::map<int, std::vector<int>> graf;
+std::map<int, float> strujeStubova; // Struja koja prolazi kroz stubove
+std::map<std::pair<int, int>, float> strujaKrozVod; // Struja kroz svaki vod
+
 struct Stub {
     int id;
     double ulaznaStruja;
@@ -54,63 +58,108 @@ struct Konekcija {
     float rastojanje;
 };
 
+std::vector<Konekcija> skupKonekcija;
+
 void napraviKonekcije(std::vector<Konekcija>& skupKonekcija, const std::vector<Vod>& vodovi, int brStubova, int trVodInd) {
-    
     if (brStubova == 0) return;
 
     for (int i = trVodInd; i < vodovi.size(); i++) {
         for (int j = vodovi[i].stubovi.size() - 1; j > 0; j--) {
-            Konekcija knkcija = {vodovi[i].stubovi[j], vodovi[i].stubovi[j - 1], 100, 0};
-            skupKonekcija.push_back(knkcija);
+            // Proveri da li konekcija već postoji
+            bool postoji = false;
+            for (const auto& k : skupKonekcija) {
+                if ((k.stub1.id == vodovi[i].stubovi[j].id && k.stub2.id == vodovi[i].stubovi[j - 1].id) ||
+                    (k.stub1.id == vodovi[i].stubovi[j - 1].id && k.stub2.id == vodovi[i].stubovi[j].id)) {
+                    postoji = true;
+                    break;
+                }
+            }
+
+            if (!postoji) {
+                Konekcija knkcija = {vodovi[i].stubovi[j], vodovi[i].stubovi[j - 1], 0, 0};
+                if(vodovi[i].stubovi[j].id != vodovi[i].stubovi[j - 1].id) {
+
+                    skupKonekcija.push_back(knkcija);
+                    graf[vodovi[i].stubovi[j].id].push_back(vodovi[i].stubovi[j - 1].id);
+                }
+                
+            }
         }
     }
 }
 
-void izracunajStruje(std::vector<Konekcija>& konekcije, int pocetniStub, double pocetnaStruja) {
-    std::map<Stub, std::vector<Stub>> graf;
-    std::map<Stub, double> struje;
-
+std::map<int, std::vector<int>> pronadjiPutanje(const std::vector<Konekcija>& konekcije) {
+    std::map<int, std::vector<int>> graf;
+    std::map<int, std::vector<int>> putanje;
 
     // Kreiranje grafa
-    for (const auto& konekcija : konekcije) {
-        graf[konekcija.stub1].push_back(konekcija.stub2);
+    for (const auto& k : konekcije) {
+        graf[k.stub1.id].push_back(k.stub2.id);
+        graf[k.stub2.id].push_back(k.stub1.id);
     }
 
-    std::cout << pocetnaStruja << std::endl;
+    // Pronalazak trafo stanice (stub sa najmanjim ID-jem)
+    int trafoStub = konekcije[0].stub1.id;
+    for (const auto& k : konekcije) {
+        if (k.stub1.id < trafoStub) trafoStub = k.stub1.id;
+        if (k.stub2.id < trafoStub) trafoStub = k.stub2.id;
+    }
 
-    // Postavljanje početne struje
-    struje[konekcije[0].stub1] = pocetnaStruja;
+    std::cout << "Trafo stanica je stub: " << trafoStub << std::endl;
 
-    // Red za BFS
-    std::queue<Stub> red;
-    red.push(konekcije[0].stub1); // Početni stub
+    // BFS za svaku putanju
+    for (const auto& par : graf) {
+        int pocetniStub = par.first;
+        std::queue<std::vector<int>> red;
+        std::map<int, bool> posecen;
 
-    // BFS za raspodelu struje
-    while (!red.empty()) {
-        Stub trenutniStub = red.front();
-        red.pop();
+        red.push({pocetniStub});
+        posecen[pocetniStub] = true;
 
-        // Broj odlaznih grana
-        int brojOdlaznihStruja = graf[trenutniStub].size();
+        while (!red.empty()) {
+            std::vector<int> trenutniPut = red.front();
+            red.pop();
 
-        if (brojOdlaznihStruja == 0) continue; // Ako nema odlaznih grana, preskoči
+            int trenutniStub = trenutniPut.back();
+            if (trenutniStub == trafoStub) {
+                putanje[pocetniStub] = trenutniPut;
+                break;
+            }
 
-        // Struja po grani
-        double strujaPoGrani = struje[trenutniStub] / brojOdlaznihStruja;
-
-        // Prolazak kroz sve odlazne grane
-        for (const auto& sused : graf[trenutniStub]) {
-            struje[sused] = strujaPoGrani;
-            red.push(sused);
-
-            // Ažuriranje struje u konekcijama
-            for (auto& konekcija : konekcije) {
-                if (konekcija.stub1.id == trenutniStub.id && konekcija.stub2.id == sused.id) {
-                    konekcija.struja = strujaPoGrani;
+            for (int sused : graf[trenutniStub]) {
+                if (!posecen[sused]) {
+                    std::vector<int> noviPut = trenutniPut;
+                    noviPut.push_back(sused);
+                    red.push(noviPut);
+                    posecen[sused] = true;
                 }
             }
         }
     }
+
+    return putanje;
+}
+
+float racunajStruju(int stub, int prethodni) {
+    float ukupnaStruja = strujeStubova[stub];
+
+    for (int sused : graf[stub]) {
+        if (sused == prethodni) continue; // Izbegavamo povratak
+
+        float strujaSuseda = racunajStruju(sused, stub);
+        ukupnaStruja += strujaSuseda;
+
+        // Ažuriranje struje u konekciji
+        for (auto& konekcija : skupKonekcija) {
+            if ((konekcija.stub1.id == stub && konekcija.stub2.id == sused) ||
+                (konekcija.stub1.id == sused && konekcija.stub2.id == stub)) {
+                konekcija.struja = strujaSuseda; // Ažuriraj struju samo jednom
+                break;
+            }
+        }
+    }
+
+    return ukupnaStruja;
 }
 
 int main() {
@@ -127,10 +176,13 @@ int main() {
     std::vector<Vod> vodovi;
 
     int stariId = -1;
+
     while (getline(mrezaFajl, linijaFajla)) {
         std::istringstream iss(linijaFajla);
         Potrosac noviPotrosac;
-        iss >> noviPotrosac.ime >> noviPotrosac.vod.id >> noviPotrosac.stub.id;
+        iss >> noviPotrosac.ime >> noviPotrosac.vod.id >> noviPotrosac.stub.id >> noviPotrosac.struja;
+
+        strujeStubova[noviPotrosac.stub.id] += noviPotrosac.struja;
 
         potrosaci.push_back(noviPotrosac);
         brPotrosaca++;
@@ -155,22 +207,38 @@ int main() {
         std::cout << std::endl;
     }
 
-    std::vector<Konekcija> skupKonekcija;
-    Stub trStub = vodovi[0].stubovi[0];
-
     int brStubova = stubovi.size();
     napraviKonekcije(skupKonekcija, vodovi, brStubova, 0);
 
-    izracunajStruje(skupKonekcija, vodovi[0].stubovi[0].id, 100);
-
-    float sum = 0;
-    for(int i = 0; i < skupKonekcija.size(); i++) {
-        std::cout << skupKonekcija[i].stub1.id << " " << skupKonekcija[i].stub2.id << std::endl;
-        std::cout << skupKonekcija[i].struja << std::endl;
-        sum += skupKonekcija[i].struja * skupKonekcija[i].struja;
+    std::map<int, std::vector<int>> putanje = pronadjiPutanje(skupKonekcija);
+    for (const auto& p : putanje) {
+        for (const auto& v : p.second) {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
     }
 
-    std::cout << sum << std::endl;
+    int trafoStub = stubovi.begin()->id;
+    racunajStruju(trafoStub, -1);
+
+    std::cout << "Struja kroz svaku konekciju:" << std::endl;
+    for (int i = 0; i < skupKonekcija.size(); i++) {
+        if(skupKonekcija[i].stub1.id == skupKonekcija[i].stub2.id) {
+            skupKonekcija[i].struja -= skupKonekcija[i].stub1.ulaznaStruja;
+        }
+    }
+
+    for(int i = 0; i < skupKonekcija.size(); i++) {
+        std::cout << "Skup konekcija izmedju " << skupKonekcija[i].stub1.id <<
+                  " i " << skupKonekcija[i].stub2.id << " nosi struju: " << skupKonekcija[i].struja << std::endl; 
+    }
+
+    float sum = 0;
+    for (const auto& konekcija : skupKonekcija) {
+        sum += konekcija.struja * konekcija.struja;
+    }
+
+    std::cout << "Ukupna snaga: " << sum << " W" << std::endl;
 
     return 0;
 }
